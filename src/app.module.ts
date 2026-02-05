9d97b713d67a6a0a7bf738feb4c03ca1a062dacb
Yash Soni <yash190068@gmail.com>
2026-02-05 21:40:03 +0530

    Base Project Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/mongoose.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { DiscoveryModule } from './discovery/discovery.module';
+import { PlatformsModule } from './platforms/platforms.module';
+import { WishlistModule } from './wishlist/wishlist.module';
+import { AffiliateModule } from './affiliate/affiliate.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { AdminModule } from './admin/admin.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    DiscoveryModule,
+    PlatformsModule,
+    WishlistModule,
+    AffiliateModule,
+    AnalyticsModule,
+    AdminModule,
+    HealthModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/platform-adapters/interfaces/platform.interface.ts
@@ -0,0 +1,17 @@
+export interface PlatformOffer {
+    productKey: string;
+    title: string;
+    image: string;
+    price: number;
+    deliveryTime?: number;
+    rating?: number;
+    noOfReviews?: number;
+    affiliateUrl: string;
+    platform: string;
+}
+
+export interface PlatformAdapter {
+    search(query: string): Promise<PlatformOffer[]>;
+    getTrending(): Promise<PlatformOffer[]>;
+    getOffersByProductKey?(productKey: string): Promise<PlatformOffer[]>;
+}
